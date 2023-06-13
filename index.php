93a6d30fc7f82657a28d7b08e3fc9dd435d6cecb
nuvolaliv <nuvolaliv@gmail.com>
2023-06-13 22:32:54 +0200

    il mio secondo commit

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-home
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>La mia Home</h1>
+</body>
+</html>
